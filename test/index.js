fd0a79ef098d8c412bf77eaf3d4833ded489c35f
blugavere <b.lugavere@gmail.com>
2017-01-04 23:17:18 -0500

    tests

--- test/index.js
+++ test/index.js
@@ -1,9 +1,25 @@
-import assert from 'assert';
-import MockMongooseModel from '../lib';
+const assert = require('assert');
+const MockMongooseModel = require('../lib');
+const sinon = require('sinon');
 
+// mocha test/index.js --watch
 describe('mock-mongoose-model', function () {
   it('should be a constructor!', function () {
     const model = new MockMongooseModel({});
-    assert(typeof model === 'object', 'we expected this package author to add actual unit tests.');
+    assert(typeof model === 'object', 'we expected this to be a constructor.');
+  });
+  it('should be able to be duped', function () {
+    const model1 = Object.assign({}, MockMongooseModel);
+    const model2 = Object.assign({}, MockMongooseModel);
+    assert(model1 !== model2, 'we expected to be able to dupe instances.');
+  });
+  it('should be able to be stubbed separately through shallow clone', function () {
+    const model1 = Object.assign({}, MockMongooseModel);
+    const model2 = Object.assign({}, MockMongooseModel);
+    sinon.stub(model1);
+    sinon.stub(model2);
+    model1.findOne();
+    assert(model1.findOne.called === true, 'we expected to be able to dupe instances.');
+    assert(model2.findOne.called === false);
   });
 });
